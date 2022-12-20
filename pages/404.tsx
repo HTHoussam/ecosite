d76f68c337d1df222f80eeb8b3864aad2c61f9a4
houssam <houssamhabbat0@gmail.com>
2022-12-20 20:44:49 +0000

    feat: 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const NoPage = () => {
+	return (
+		<div>
+			<h1 className='text-center font-bold'>404 page</h1>
+			<p className='text-center font-semibold'>Page not found</p>
+		</div>
+	)
+}
+export default NoPage

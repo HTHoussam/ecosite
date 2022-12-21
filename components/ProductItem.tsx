86dfd842a5150d814eb976c49f4338f22f837ea8
houssam <houssamhabbat0@gmail.com>
2022-12-21 13:08:35 +0000

    feat: add to cart functionnality in ProductItem

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,8 +1,23 @@
 import Link from 'next/link'
+import router from 'next/router'
+import { useContext } from 'react'
 import { Product } from '../pages/types/types'
 import { titleToSlug } from '../utils/helpers'
+import { Store } from '../utils/store'
 
 const ProductItem = ({ product }: { product: Product }) => {
+	const { state, dispatch } = useContext(Store)
+	const addToCartHandler = () => {
+		const existItem = state.cart.cartItems.find(
+			(x: Product) => x.id === product.id
+		)
+		const quantity = existItem ? existItem.quantity + 1 : 1
+		if (product.stock < quantity) {
+			return alert('out of stock, Sorry!')
+		}
+		dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } })
+		router.push('/cart')
+	}
 	return (
 		<div className='card'>
 			<Link href={`/product/${product.id}`}>
@@ -18,7 +33,10 @@ const ProductItem = ({ product }: { product: Product }) => {
 				</Link>
 				<p className='mb-2'>{product.brand}</p>
 				<p>${product.price}</p>
-				<button className='primary-button' type='button'>
+				<button
+					className='primary-button'
+					onClick={addToCartHandler}
+					type='button'>
 					Add to cart
 				</button>
 			</div>

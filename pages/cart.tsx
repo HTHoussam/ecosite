1d6ba44e3a7cbdb466f152cd6f57a9c51d67283b
houssam <houssamhabbat0@gmail.com>
2022-12-21 13:55:51 +0000

    refactor: cart page code + add dynamic load

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,10 +1,10 @@
+import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import Layout from '../components/Layout'
-import { Store } from '../utils/store'
-import { useRouter } from 'next/router'
 import CartTable from '../components/pages/cart/CartTable'
 import CartTotal from '../components/pages/cart/CartTotal'
+import { Store } from '../utils/store'
 
 const CartPage = () => {
 	const { state, dispatch } = useContext(Store)
@@ -22,59 +22,6 @@ const CartPage = () => {
 				) : (
 					<div className='grid md:grid-cols-4 md:gap-5'>
 						<div className='overflow-x-auto md:col-span-3'>
-							{/* <table className='min-w-full'>
-								<thead className='border-b'>
-									<tr>
-										<th className='px-5 text-left'>Item</th>
-										<th className='p-5 text-right'>Quantity</th>
-										<th className='p-5 text-right'>Price</th>
-										<th className='p-5'>Action</th>
-									</tr>
-								</thead>
-								<tbody>
-									{cartItems.map((item: Product) => (
-										<tr key={item.id} className='border-b'>
-											<td>
-												<Link href={`/product/${item.id}`}>
-													<Image
-														src={item.images[0]}
-														alt={item.title}
-														width={50}
-														height={50}></Image>
-													&nbsp;
-													{item.title}
-												</Link>
-											</td>
-											<td className='p-5 text-right'>
-												<select
-													className='bg-white w-12'
-													name='quantity'
-													id='quantity'
-													value={item.quantity}
-													onChange={(e) =>
-														updateCartHandler(item, e.target.value)
-													}>
-													{' '}
-													{[...Array(item.stock).keys()].map((x) => (
-														<option key={x + 1} value={x + 1}>
-															{x + 1}
-														</option>
-													))}
-												</select>
-											</td>
-											<td className='p-5 text-right'>${item.price}</td>
-											<td className='p-5 text-center'>
-												<button
-													type='button'
-													onClick={() => removeItemHandler(item)}>
-													<XCircleIcon height={25} width={25}></XCircleIcon>
-												</button>
-											</td>
-											<td></td>
-										</tr>
-									))}
-								</tbody>
-							</table> */}
 							<CartTable cartItems={cartItems} />
 							{/* here table */}
 						</div>
@@ -86,4 +33,4 @@ const CartPage = () => {
 	)
 }
 
-export default CartPage
+export default dynamic(() => Promise.resolve(CartPage), { ssr: false })

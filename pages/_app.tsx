90014479e8eefada93004cf3ffde227e26fa0c20
houssam <houssamhabbat0@gmail.com>
2022-12-23 16:37:53 +0000

    feat: wrapped _app component with auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,42 @@
-import { SessionProvider } from 'next-auth/react'
+import { NextComponentType } from 'next'
+import { SessionProvider, useSession } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 import { StoreProvider } from '../utils/store'
 
+type CustomAppProps = AppProps & {
+	Component: NextComponentType & { auth?: boolean } // add auth type
+}
 export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
-}: AppProps) {
+}: CustomAppProps) {
 	return (
 		<SessionProvider session={session}>
 			<StoreProvider>
-				<Component {...pageProps} />
+				{Component.auth ? (
+					<Auth>
+						<Component {...pageProps} />
+					</Auth>
+				) : (
+					<Component {...pageProps} />
+				)}
 			</StoreProvider>
 		</SessionProvider>
 	)
+}
+
+function Auth({ children }: { children: JSX.Element }) {
+	const router = useRouter()
+	const { status } = useSession({
+		required: true,
+		onUnauthenticated() {
+			router.push('/unauthorized?message=login required')
+		},
+	})
+	if (status === 'loading') {
+		return <div>Loading...</div>
+	}
+	return children
 }

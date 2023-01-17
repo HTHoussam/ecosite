9d425029951f357a921d7f01db5849745f4f3a05
houssam <houssamhabbat0@gmail.com>
2023-01-17 16:20:19 +0000

    fix: shipping validation problem

--- pages/shipping.tsx
+++ pages/shipping.tsx
@@ -2,10 +2,12 @@ import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
 import { useContext, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import Layout from '../components/Layout'
 import CheckoutWizard from '../components/pages/shipping/CheckoutWizard'
 import ShippingInput from '../components/pages/shipping/ShippingInput'
 import { ShippingAddressType } from '../types/types'
+import { getError } from '../utils/helpers'
 import { Store } from '../utils/store'
 const ShippingPage = () => {
 	const router = useRouter()
@@ -17,7 +19,7 @@ const ShippingPage = () => {
 		register,
 		setValue,
 		formState: { errors },
-	} = useForm<ShippingAddressType>()
+	} = useForm<ShippingAddressType>({ mode: 'all' })
 	useEffect(() => {
 		setValue('fullName', shippingAddress.fullName)
 		setValue('address', shippingAddress.address)
@@ -28,22 +30,25 @@ const ShippingPage = () => {
 	const shippingInputs: Array<{
 		label: string
 		id: string
+		type: string
 		inputRegisterCallBack: any
 		inputError: any
 	}> = [
 		{
 			label: 'full name',
 			id: 'fullName',
+			type: 'text',
 			inputRegisterCallBack: register('fullName', {
-				required: 'Please enter full name',
+				required: 'This field is required',
 			}),
 			inputError: errors.fullName,
 		},
 		{
 			label: 'Address',
 			id: 'address',
+			type: 'text',
 			inputRegisterCallBack: register('address', {
-				required: 'Please enter address',
+				required: 'This field is required',
 				minLength: { value: 3, message: 'Address is more than 2 characters' },
 			}),
 			inputError: errors.address,
@@ -51,24 +56,28 @@ const ShippingPage = () => {
 		{
 			label: 'City',
 			id: 'city',
+			type: 'text',
 			inputRegisterCallBack: register('city', {
-				required: 'Please enter city',
+				required: 'This field is required',
 			}),
 			inputError: errors.city,
 		},
 		{
 			label: 'Postal Code',
 			id: 'postalCode',
+			type: 'number',
 			inputRegisterCallBack: register('postalCode', {
-				required: 'Please enter postal code',
+				required: 'This field is required',
+				valueAsNumber: true,
 			}),
 			inputError: errors.postalCode,
 		},
 		{
 			label: 'Country',
 			id: 'country',
+			type: 'text',
 			inputRegisterCallBack: register('country', {
-				required: 'Please enter country',
+				required: 'This field is required',
 			}),
 			inputError: errors.country,
 		},
@@ -104,8 +113,11 @@ const ShippingPage = () => {
 					},
 				})
 			)
+			if (Object.keys(errors).length === 0) {
+				router.push('/payment')
+			}
 		} catch (error) {
-			console.log(error)
+			toast.error(getError(error))
 		}
 	}
 	return (
@@ -116,9 +128,9 @@ const ShippingPage = () => {
 					className='mx-auto max-w-screen-md'
 					onSubmit={handleSubmit(shippingSubmitHandler)}>
 					<h1 className='mb-4 text-xl'>Shipping Address</h1>
-
 					{shippingInputs.map((i) => (
 						<ShippingInput
+							type={i.type}
 							label={i.label}
 							inputError={i.inputError}
 							inputRegisterCallBack={i.inputRegisterCallBack}
@@ -127,13 +139,7 @@ const ShippingPage = () => {
 						/>
 					))}
 					<div className='mb-4 flex justify-between'>
-						<button
-							className='primary-button'
-							onClick={() => {
-								router.push('/payment')
-							}}>
-							Checkout
-						</button>
+						<button className='primary-button'>Checkout</button>
 					</div>
 				</form>
 			</>
